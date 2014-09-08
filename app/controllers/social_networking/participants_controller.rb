6d1c1608aa5dcb520aebfc52ae6991b176db7735
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-09-08 15:30:59 -0500

    refactor ParticipantsController

--- app/controllers/social_networking/participants_controller.rb
+++ app/controllers/social_networking/participants_controller.rb
@@ -4,26 +4,26 @@ module SocialNetworking
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
     def index
-      render json: (Participant.all.map do |p|
-        {
-          id: p.id,
-          username: p.email,
-          lastLogin: p.last_sign_in_at
-        }
-      end)
+      participants = Participant.all
+
+      render json: participants.map { |p| model_json(p) }
     end
 
     def show
-      @participant = Participant.find(params[:id])
+      participant = Participant.find(params[:id])
 
-      render json: {
-        id: @participant.id,
-        username: @participant.email,
-        lastLogin: @participant.last_sign_in_at
-      }
+      render json: model_json(participant)
     end
 
     private
+
+    def model_json(model)
+      {
+        id: model.id,
+        username: model.email,
+        lastLogin: model.last_sign_in_at
+      }
+    end
 
     def record_not_found
       render json: { error: "participant not found" }, status: 404
