30c8ff5a6b5def23e8d9e02d0336e828cbbfc081
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-09-17 14:42:32 -0500

    fix inconsistent migrations

--- db/migrate/20140916163621_add_profile_to_social_networking_profile_answers.rb
+++ db/migrate/20140916163621_add_profile_to_social_networking_profile_answers.rb
@@ -1,5 +1,23 @@
 class AddProfileToSocialNetworkingProfileAnswers < ActiveRecord::Migration
   def change
     add_reference :social_networking_profile_answers, :social_networking_profile, index: false
+
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          ALTER TABLE social_networking_profile_answers
+            ADD CONSTRAINT fk_profile_answers_profiles
+            FOREIGN KEY (social_networking_profile_id)
+            REFERENCES social_networking_profiles(id)
+        SQL
+      end
+
+      dir.down do
+        execute <<-SQL
+          ALTER TABLE social_networking_profile_answers
+            DROP CONSTRAINT IF EXISTS fk_profile_answers_profiles
+        SQL
+      end
+    end
   end
 end
