5173b63d14031fe07e5bf3ef2fe23ec94d5bdbaf
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-10-05 12:38:55 -0500

    optimized queries

--- app/controllers/social_networking/homes_controller.rb
+++ app/controllers/social_networking/homes_controller.rb
@@ -5,10 +5,12 @@ module SocialNetworking
       @action_items = ActionItem.for(current_participant)
       @feed_items = (
         Serializers::OnTheMindStatementSerializer
-          .from_collection(OnTheMindStatement.all) +
-        Serializers::GoalSerializer.from_collection(Goal.all) +
-        Serializers::NudgeSerializer.from_collection(Nudge.all) +
-        Serializers::SharedItemSerializer.from_collection(SharedItem.all)
+          .from_collection(OnTheMindStatement.includes(:comments)) +
+        Serializers::GoalSerializer.from_collection(Goal.includes(:comments)) +
+        Serializers::NudgeSerializer
+          .from_collection(Nudge.includes(:comments)) +
+        Serializers::SharedItemSerializer
+          .from_collection(SharedItem.includes(:item, :comments))
       )
       @member_profiles = []
     end
