99ba42a6ec6f1f17a89bd885d7a2dd6efbcf9429
Eric Schlange <eric.schlange@gmail.com>
2014-10-07 15:35:33 -0500

    Added profile images to the asset folder, fixed a js standards complaint which is causing the travis build to fail.

--- app/assets/javascripts/social_networking/resources/nudges-resource.js
+++ app/assets/javascripts/social_networking/resources/nudges-resource.js
@@ -5,7 +5,7 @@
     var NudgeResource = $resource('/social_networking/nudges/:id',
       { id: '@id', recipient_id: '@recipient_id'});
 
-    function Nudge() {};
+    function Nudge() {}
 
     Nudge.create = function(attributes) {
       var nudge = new NudgeResource({
