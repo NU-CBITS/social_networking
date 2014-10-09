9e25031510eeb45951e97d655095ff807dc45ac8
eschlange <eric.schlange@gmail.com>
2014-10-09 11:32:05 -0500

    Icons can now be selected and saved from the profile edit page.

--- app/controllers/social_networking/profile_icon_controller.rb
+++ app/controllers/social_networking/profile_icon_controller.rb
@@ -2,16 +2,25 @@ module SocialNetworking
   # Manage profile icons.
   class ProfileIconController < ApplicationController
 
+    skip_before_filter :verify_authenticity_token
+
     def save
-      profile = Profiles.find(profile_icon_params[:profile_id])
+      profile = Profile.find(profile_icon_params[:profile_id])
       profile.icon_name = profile_icon_params[:icon_name]
-      profile.save
+
+      if profile.save
+        respond_to do |format|
+          format.html { render nothing: true, status: :accepted }
+          format.js   { render nothing: true, status: :accepted }
+          format.json { render nothing: true, status: :accepted }
+        end
+      end
     end
 
     private
 
     def profile_icon_params
-      params.permit(:profile_id, :icon_name)
+      params.require(:profile).permit(:profile_id, :icon_name)
     end
   end
 end
