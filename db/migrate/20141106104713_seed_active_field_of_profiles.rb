37f8bbf931cd3e1c8c97af8877c97e635c1a6bda
Eric Schlange <eric.schlange@gmail.com>
2014-11-06 11:04:32 -0600

    Fixed logic of migration.

--- db/migrate/20141106104713_seed_active_field_of_profiles.rb
+++ db/migrate/20141106104713_seed_active_field_of_profiles.rb
@@ -1,7 +1,9 @@
 class SeedActiveFieldOfProfiles < ActiveRecord::Migration
   def change
-    Profile.each do |profile|
+    Profile.all.each do |profile|
       profile.update(active: true)
     end
   end
 end
+
+
