4a2e93f4f8e97bccf8d9bdf2e76ecf24fbd1164b
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-11-13 09:41:39 -0600

    refactored sms concern to only deliver on staging and production

--- app/controllers/concerns/sms.rb
+++ app/controllers/concerns/sms.rb
@@ -13,18 +13,16 @@ module Sms
     logger.info("INFO BEFORE: SMS notification sent \
                  to:" + recipient.phone_number)
     if recipient.is_a?(Participant)
-      client = Twilio::REST::Client.new(
-        Rails.application.config.twilio_account_sid,
-        Rails.application.config.twilio_auth_token)
-      account = client.account
-      account.messages.create(
-        from:
-          "#{Rails.application.config.twilio_account_telephone_number}",
-        to:
-          "+#{recipient.phone_number}",
-        body:
-          message_body
-      )
+      if Rails.env.staging? || Rails.env.production?
+        client = Twilio::REST::Client.new(
+          Rails.application.config.twilio_account_sid,
+          Rails.application.config.twilio_auth_token)
+        account = client.account
+        account.messages.create(message_attributes(recipient, message_body))
+      else
+        logger.info("INFO - SMS attributes: " \
+                    "#{ message_attributes(recipient, message_body) }")
+      end
     else
       logger.error "Error: Expected recipient to be a Participant."
     end
@@ -32,4 +30,17 @@ module Sms
                  to:" + recipient.phone_number)
   end
   # rubocop:enable Metrics/AbcSize
+
+  private
+
+  def message_attributes(recipient, body)
+    {
+      from:
+        "#{ Rails.application.config.twilio_account_telephone_number }",
+      to:
+        "+#{ recipient.phone_number }",
+      body:
+        body
+    }
+  end
 end
