5bd4ff4804c191c5308d091c3ac248d5b0821214
Eric Schlange <eric.schlange@gmail.com>
2014-12-29 18:40:56 -0600

    [#84949534] Modified migration to remove population of participant_id on shared items if the associated item is nil.

--- db/migrate/20141223203721_add_participant_id_to_social_networking_shared_items_data_migration.rb
+++ db/migrate/20141223203721_add_participant_id_to_social_networking_shared_items_data_migration.rb
@@ -1,7 +1,9 @@
 class AddParticipantIdToSocialNetworkingSharedItemsDataMigration < ActiveRecord::Migration
   def change
     SocialNetworking::SharedItem.where(participant_id: nil).each do |shared_item|
-      shared_item.update(participant_id: shared_item.item.participant.id)
+      if shared_item.item
+        shared_item.update(participant_id: shared_item.item.participant.id)
+      end
     end
   end
 end
