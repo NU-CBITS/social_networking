f5cd40a07d0b15719a3e727f214bbe5220671c54
gwensmu <gwen.smuda@gmail.com>
2015-03-09 09:28:05 -0500

    Update specs for style
    [#89516832]

--- spec/models/social_networking/comment_spec.rb
+++ spec/models/social_networking/comment_spec.rb
@@ -7,39 +7,32 @@ module SocialNetworking
     let(:goal) { social_networking_goals(:participant2_goal_alpha) }
 
     it "should return the number of comments made today" do
-      count = Comment.for_today.count
-
-      Comment.create(
-        participant_id: participant1.id,
-        item_id: goal.id,
-        text: "love this test",
-        item_type: "SocialNetworking::Goal",
-        created_at: Date.today
-      )
-
-      expect(Comment.for_today.count).to eq(count + 1)
+      expect do
+        Comment.create(
+          participant_id: participant1.id,
+          item: goal,
+          text: "love this test",
+          created_at: Date.today
+        )
+      end.to change { Comment.for_today.count }.by(1)
     end
 
     it "should return the number of comments made in the past seven days" do
-      count = Comment.for_week.count
+      expect do
+        Comment.create(
+          participant_id: participant1.id,
+          item: goal,
+          text: "love this test",
+          created_at: Date.today - 1.days
+        )
 
-      Comment.create(
-        participant_id: participant1.id,
-        item_id: goal.id,
-        text: "love this test",
-        item_type: "SocialNetworking::Goal",
-        created_at: Date.today - 1.days
-      )
-
-      Comment.create(
-        participant_id: participant1.id,
-        item_id: goal.id,
-        text: "love this test",
-        item_type: "SocialNetworking::Goal",
-        created_at: Date.today - 8.days
-      )
-
-      expect(Comment.for_week.count).to eq(count + 1)
+        Comment.create(
+          participant_id: participant1.id,
+          item: goal,
+          text: "love this test",
+          created_at: Date.today - 8.days
+        )
+      end.to change { Comment.for_week.count }.by(1)
     end
   end
 end
