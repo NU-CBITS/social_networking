55216f886afd7bfc60f43070059b405bad65cf11
Eric Schlange <eric.schlange@gmail.com>
2015-05-21 14:41:31 -0500

    Fixed scoping issue with exposure of Notice functions.
    [#95080280]

--- app/assets/javascripts/notices.js
+++ app/assets/javascripts/notices.js
@@ -1,86 +1,92 @@
-var Notice = (function() {
-  "use strict";
-  var toastTimeout = 7000;
+"use strict";
 
-  var actionNotice = function(actionType, description, participantId) {
-    behaviorPostNotice(actionType, description, participantId);
-    setTimeout(
-      function () {
-        incentivePostNotice(actionType, description, participantId);
-      }, 3000);
-  };
+(function() {
+  function Notice() {
+    var toastTimeout = 7000;
 
-  var incentivePostNotice = function(actionType, description, participantId) {
-    $.ajax({
-      type: "POST",
-      url: "/participant_incentives/complete",
-      data: {
-        "participant_id": participantId,
-        "action_type": actionType
-      },
-      success: function (data, status) {
-        if ('success' === status) {
-          incentivRealTimeToast(description);
+    this.actionNotice = function(actionType, description, participantId) {
+      var self = this;
+      this.behaviorPostNotice(actionType, description, participantId);
+      setTimeout(
+        function () {
+          self.incentivePostNotice(actionType, description, participantId);
+        }, 3000);
+    };
+
+    this.incentivePostNotice = function(actionType, description, participantId) {
+      var self = this;
+      $.ajax({
+        type: "POST",
+        url: "/participant_incentives/complete",
+        data: {
+          "participant_id": participantId,
+          "action_type": actionType
+        },
+        success: function (data, status) {
+          if ('success' === status) {
+            self.incentivRealTimeToast(description);
+          }
         }
-      }
-    });
-  };
-
-  var behaviorPostNotice = function(actionType, description, participantId) {
-    $.ajax({
-      type: "POST",
-      url: "/participant_behaviors/complete",
-      data: {
-        "participant_id": participantId,
-        "action_type": actionType
-      },
-      success: function (data, status) {
-        if ('success' === status) {
-          behaviorRealTimeToast(description);
-        }
-      }
-    });
-  };
-
-  var incentiveToastView = function(incentiveId, description) {
-    realTimeToast("incentive", description);
-    setTimeout(
-      function () {
-        $.ajax({
-          type: "PATCH",
-          url: "/participant_incentives/" + incentiveId + "/complete"
-        });
-      }, toastTimeout);
-  };
-
-  var behaviorToastView = function(behaviorId, description) {
-    realTimeToast("behavior", description);
-    setTimeout(
-      function () {
-        $.ajax({
-          type: "PATCH",
-          url: "/participant_behaviors/" + behaviorId + "/complete"
-        });
-      }, toastTimeout);
-  };
-
-  var incentivRealTimeToast = function(description) {
-    realTimeToast('incentive', description);
-  };
-
-  var behaviorRealTimeToast = function(description) {
-    realTimeToast('behavior', description);
-  };
-
-  var realTimeToast = function(type, description) {
-    $.toaster(
-      {
-        title: 'Congratulations',
-        priority: 'success',
-        message: 'you completed the ' + type + ': ' + description,
-        settings: {timeout: toastTimeout}
       });
-  };
+    };
 
-  return this;
+    this.behaviorPostNotice = function(actionType, description, participantId) {
+      var self = this;
+      $.ajax({
+        type: "POST",
+        url: "/participant_behaviors/complete",
+        data: {
+          "participant_id": participantId,
+          "action_type": actionType
+        },
+        success: function (data, status) {
+          if ('success' === status) {
+            self.behaviorRealTimeToast(description);
+          }
+        }
+      });
+    };
+
+    this.incentiveToastView = function(incentiveId, description) {
+      this.realTimeToast("incentive", description);
+      setTimeout(
+        function () {
+          $.ajax({
+            type: "PATCH",
+            url: "/participant_incentives/" + incentiveId + "/complete"
+          });
+        }, toastTimeout);
+    };
+
+    this.behaviorToastView = function(behaviorId, description) {
+      this.realTimeToast("behavior", description);
+      setTimeout(
+        function () {
+          $.ajax({
+            type: "PATCH",
+            url: "/participant_behaviors/" + behaviorId + "/complete"
+          });
+        }, toastTimeout);
+    };
+
+    this.incentivRealTimeToast = function(description) {
+      this.realTimeToast('incentive', description);
+    };
+
+    this.behaviorRealTimeToast = function(description) {
+      this.realTimeToast('behavior', description);
+    };
+
+    this.realTimeToast = function(type, description) {
+      $.toaster(
+        {
+          title: 'Congratulations',
+          priority: 'success',
+          message: 'you completed the ' + type + ': ' + description,
+          settings: {timeout: toastTimeout}
+        });
+    };
+  }
+
+  window.Notice = new Notice();
 }());
