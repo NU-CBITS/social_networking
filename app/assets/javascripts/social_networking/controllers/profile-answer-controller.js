0c3e1aba9d8cbbf4188d34c8dac46e696b463646
michaelwehrley <michaelwehrley@gmail.com>
2015-07-10 17:10:14 -0500

    Fix Profile Page Bugs
    * When participant adds no text, default is set to empty string "".
    * Error message is added and fields change back to previous answer.
    * After error message, field is no longer focused.
    * After creation, profile answer updates the collection in memory.
    * Dynamic h3 for alerting user to complete profile.
    * h3 disappears once one question has been ansered.
    * Add spec to test when `create` for question-profile-answers succeed or fail
    * Remove unnecessary controller calls from view.
    
    [Finished: #98849186]
    [Finished: #97428814]
    
    asdf

--- app/assets/javascripts/social_networking/controllers/profile-answer-controller.js
+++ app/assets/javascripts/social_networking/controllers/profile-answer-controller.js
@@ -2,42 +2,58 @@
     "use strict";
 
     // Provide interaction with a participant's profile answers to profile questions.
-    function ProfileAnswerCtrl(ProfileAnswers) {
-      var self = this;
-      self._answerResource = ProfileAnswers;
-      self.answerModels = {};
-      self._answerStates = {};
+    function ProfileAnswerCtrl($window, ProfileAnswers) {
+      this._answerResource = ProfileAnswers;
+      this.answerModels = {};
+      this._answerStates = {};
+      this.$window = $window;
     }
 
-    ProfileAnswerCtrl.prototype.storeAnswer = function(profileId, questionId, controller) {
-      this._answerResource.getOne(profileId, questionId)
+    ProfileAnswerCtrl.prototype.storeAnswer = function(profileId, questionId) {
+      var self = this;
+      this
+        ._answerResource.getOne(profileId, questionId)
         .then(function(profileAnswer) {
-          controller.answerModels[questionId] = profileAnswer;
-          controller.setModel(profileAnswer, controller);
-          controller._answerStates[questionId] = {};
-          controller._answerStates[questionId].editable = false;
+          self.answerModels[questionId] = profileAnswer;
+          self.setModel(profileAnswer);
+          self._answerStates[questionId] = {};
+          self._answerStates[questionId].editable = false;
         })
         .catch(function(error) {
-          controller.answerModels[questionId] = {};
+          self.answerModels[questionId] = {};
           var answerModel = {};
           answerModel.profile_id = profileId;
           answerModel.profile_question_id = questionId;
-          controller.answerModels[questionId] = answerModel;
-          controller._answerStates[questionId] = {};
-          controller._answerStates[questionId].editable = true;
+          self.answerModels[questionId] = answerModel;
+          self._answerStates[questionId] = {};
+          self._answerStates[questionId].editable = true;
         });
     };
 
-    ProfileAnswerCtrl.prototype.setModel = function(answer, controller) {
+    ProfileAnswerCtrl.prototype.ifAnswered = function(questions) {
+      var self, states = [];
+
+      self = this;
+      angular.forEach(questions, function(value) {
+        var reponse = self.answerModels[value.id];
+        states.push(reponse && !!reponse.answer_text);
+      });
+      return states.indexOf(true) !== -1;
+    };
+
+    ProfileAnswerCtrl.prototype.setModel = function(answer) {
       var answerModel = {};
+
       answerModel.id = answer.id;
       answerModel.profile_id = answer.profile_id;
       answerModel.profile_question_id = answer.profile_question_id;
       answerModel.answer_text = answer.answer_text;
-      controller.answerModels[answer.profile_question_id] = answerModel;
+      this.answerModels[answer.profile_question_id] = answerModel;
     };
 
-    ProfileAnswerCtrl.prototype.getModel = function(question_id, controller) { return controller.answerModels[question_id]; };
+    ProfileAnswerCtrl.prototype.getModel = function(question_id) {
+      return this.answerModels[question_id];
+    };
 
     // Initiate answer editor interface.
     ProfileAnswerCtrl.prototype.edit = function(question_id) {
@@ -46,33 +62,43 @@
 
     // Exit answer editor interface.
     ProfileAnswerCtrl.prototype.cancelEdit = function(question_id) {
-        this._answerStates[question_id].editable = false;
+      this._answerStates[question_id].editable = false;
     };
 
     // Persist a profile from the form.
-    ProfileAnswerCtrl.prototype.save = function(profileId, questionId, controller) {
+    ProfileAnswerCtrl.prototype.save = function(profileId, questionId) {
+      var answerModel, self = this;
 
-      var answerModel = controller.getModel(questionId, controller);
+      answerModel = this.getModel(questionId);
       if (answerModel.id === undefined) {
         answerModel.profile_id = profileId;
         answerModel.profile_question_id = questionId;
-        controller._answerResource.create(answerModel).then(function() {
-          controller._answerStates[questionId].editable = false;
+        this._answerResource.create(answerModel).then(function() {
+          self.storeAnswer(profileId, questionId);
         }).catch(function(message) {
-        controller.error = message.error;
-      });
+          self.$window.confirm("There was an error: " + message.data.error + ".");
+          self._answerResource.getOne(profileId, questionId)
+            .then(function(profileAnswer) {
+              answerModel.answer_text = profileAnswer.answer_text;
+            })
+            .finally(function(error) {
+              self._answerStates[questionId].editable = false;
+            });
+        });
       } else {
         answerModel.profile_id = profileId;
         answerModel.profile_question_id = questionId;
-        controller._answerResource.update(answerModel).then(function() {
-          controller._answerStates[questionId].editable = false;
+        this._answerResource
+        .update(answerModel)
+        .then(function() {
+          self._answerStates[questionId].editable = false;
         }).catch(function(message) {
-          controller.error = message.error;
+          self.$window.confirm("There was an error: " + message.data.error + ".");
         });
       }
     };
 
     // Create a module and register the controllers.
     angular.module('socialNetworking.controllers')
-      .controller('ProfileAnswerCtrl', ['ProfileAnswers', ProfileAnswerCtrl]);
+      .controller('ProfileAnswerCtrl', ['$window', 'ProfileAnswers', ProfileAnswerCtrl]);
 })();
