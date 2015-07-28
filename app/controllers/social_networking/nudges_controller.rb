c278d6dfb350ee7d50e96d254d4960be1e851265
michaelwehrley <michaelwehrley@gmail.com>
2015-07-28 17:08:43 -0500

    Refactor Updates
    * Display custom alert message from controller.
    * Clean up controller methods.

--- app/controllers/social_networking/nudges_controller.rb
+++ app/controllers/social_networking/nudges_controller.rb
@@ -2,6 +2,7 @@
 module SocialNetworking
   # Manage Nudges.
   class NudgesController < ApplicationController
+    before_action :set_recipient
     include Sms
 
     def index
@@ -13,9 +14,10 @@ module SocialNetworking
     def create
       @nudge = Nudge.new(sanitized_params)
 
-      if @nudge.save
-        notify
-        render json: Serializers::NudgeSerializer.new(@nudge).to_serialized
+      if !@recipient.contact_preference
+        render json: { message: "This participant can't be nudged." }
+      elsif @nudge.save
+        render json: { message: "Nudge sent!" }
       else
         render json: { error: model_errors }, status: 400
       end
@@ -33,18 +35,14 @@ module SocialNetworking
     # Select message from list, determine contact preference, then
     # trigger the notification based on the preference.
     def notify
-      recipient = Participant.find(sanitized_params[:recipient_id])
-
-      case recipient.contact_preference
+      case @recipient.contact_preference
       when "email"
-        send_notify_email(@nudge, message_body)
-      when "sms"
-        send_sms(recipient, message_body) if recipient.phone_number.present?
-      when "phone"
-        send_sms(recipient, message_body) if recipient.phone_number.present?
+        send_notify_email
+      when "sms", "phone"
+        send_sms(@recipient, message_body) if @recipient.phone_number.present?
       else
         logger.error "ERROR: contact preference is not set for \
-        participant with ID: #{recipient.id}"
+        participant with ID: #{@recipient.id}"
       end
     end
 
@@ -53,12 +51,16 @@ module SocialNetworking
     end
 
     # Trigger nudge notification email
-    def send_notify_email(nudge, message_body)
+    def send_notify_email
       NudgeMailer.nudge_email_alert(
-        Participant.find(nudge.recipient_id),
+        @recipient,
         message_body,
         "You've been NUDGED on "\
-        "#{t 'application_name', default: 'ThinkFeelDo' }").deliver
+        "#{t('application_name', default: 'ThinkFeelDo')}").deliver
+    end
+
+    def set_recipient
+      @recipient = Participant.find(sanitized_params[:recipient_id])
     end
 
     def message_body
