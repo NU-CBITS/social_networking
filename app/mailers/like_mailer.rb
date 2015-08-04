ba20d05456a6468bf624e0422de0bb2b402f8034
michaelwehrley <michaelwehrley@gmail.com>
2015-08-04 10:15:01 -0500

    Update Params of Mailer
    * Add default parameter to nil.
    * Remove logger statements.
    
    [Finished: #100522400]

--- app/mailers/like_mailer.rb
+++ app/mailers/like_mailer.rb
@@ -1,9 +1,9 @@
 # LikeMailer is used to notify recipients of like applied to their shared items.
 class LikeMailer < ActionMailer::Base
   # Send trigger an email that alerts an item's creator of a like.
-  def like_email_alert(receiving_participant, message_body, *subject)
+  def like_email_alert(receiving_participant, message_body, subject = nil)
     @message_body = message_body
-    subject = subject.empty? ? message_body : subject
+    subject = subject.nil? ? message_body : subject
     mail(to: receiving_participant.email, subject: subject).deliver
   end
 end
