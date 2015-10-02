545907cb6ded33ed260f90da27c4d6ab7ac46f97
michaelwehrley <michaelwehrley@gmail.com>
2015-10-02 15:55:13 -0500

    Update Views to Hide Admin Timestamp
    * Profile views do not display timestamp if participant is admin
    * Add `transparent` CSS class
    
    [#104565788]

--- app/assets/javascripts/social_networking/directives.js
+++ app/assets/javascripts/social_networking/directives.js
@@ -5,7 +5,9 @@
   function profileStatus() {
     return {
       template: '<h2>{{ profile.username }}</h2>' +
-                '<h3>Last seen: {{ profile.latestAction | timeFromNow }}</h3>'
+                '<h3 ng-class="profile.isAdmin ? \'transparent\' : \'\'">' +
+                  'Last seen: {{ profile.latestAction | timeFromNow }}' +
+                '</h3>'
     };
   }
 
