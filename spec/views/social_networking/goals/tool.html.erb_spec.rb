315998ee1faba897c1dd2a023edfbf6b07f24477
michaelwehrley <michaelwehrley@gmail.com>
2015-12-11 13:16:37 -0600

    Add Tool Help Text
    * Allow HTML to be defined for goal popovers
    * Add goal/achieve text
    * Update with spec
    
    [#106226936]

---
+++ spec/views/social_networking/goals/tool.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+module SocialNetworking
+  RSpec.describe "social_networking/goals/tool.html.erb",
+                 type: :view do
+    describe "Viewing ACHIEVE tool" do
+      before do
+        allow(view)
+          .to receive(:current_participant)
+          .and_return(
+            instance_double(
+              Participant,
+              id: 1,
+              active_membership_end_date: Date.new))
+        allow(view)
+          .to receive(:goals)
+      end
+
+      it "gives detailed description of how to use the goal" do
+        render
+
+        expect(rendered)
+          .to match "The ACHIEVE tool helps you set goals."
+      end
+    end
+  end
+end
